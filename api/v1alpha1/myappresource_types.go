0057749ee0033f671c4b7312af877840dc579f9f
Domenic Bove <domenicbove@gmail.com>
2023-06-22 08:39:23 -0700

    scaffolded out base operator

---
+++ api/v1alpha1/myappresource_types.go
@@ -0,0 +1,77 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// MyAppResourceSpec defines the desired state of MyAppResource
+type MyAppResourceSpec struct {
+	// +optional
+	// +kubebuilder:default=1
+	// +kubebuilder:validation:Minimum=0
+	// ReplicaCount sets the pod replicas for the PodInfo Deployment.
+	ReplicaCount *int32 `json:"replicaCount,omitempty"`
+
+	// TODO resources
+
+	// +optional
+	Image Image `json:"image,omitempty"`
+
+	UI UI `json:"ui"`
+
+	// TODO redis
+}
+
+// Image describes the PodInfo Container image.
+type Image struct {
+	// Repository sets the PodInfo Container image repository.
+	// +kubebuilder:default=ghcr.io/stefanprodan/podinfo
+	// +optional
+	Repository string `json:"repository,omitempty"`
+
+	// Tag sets the PodInfo Container image tag.
+	// +kubebuilder:default=latest
+	// +optional
+	Tag string `json:"tag,omitempty"`
+}
+
+// UI describes the PodInfo Container UI settings.
+type UI struct {
+	// Repository sets the PodInfo UI color.
+	// TODO validations, should look like "#34577c"
+	Color string `json:"color"`
+
+	// Message sets the PodInfo UI message.
+	Message string `json:"message"`
+}
+
+// MyAppResourceStatus defines the observed state of MyAppResource
+type MyAppResourceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// MyAppResource is the Schema for the myappresources API
+type MyAppResource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MyAppResourceSpec   `json:"spec,omitempty"`
+	Status MyAppResourceStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// MyAppResourceList contains a list of MyAppResource
+type MyAppResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MyAppResource `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&MyAppResource{}, &MyAppResourceList{})
+}
