50344c0e55c70bef89d5d97a8347b439cd0fd1b9
Domenic Bove <domenicbove@gmail.com>
2023-06-23 12:21:21 -0700

    added ut for redis deployment and service

--- internal/controller/myappresource_controller_test.go
+++ internal/controller/myappresource_controller_test.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -11,8 +12,11 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
-	myv1alpha1 "github.com/domenicbove/angi/api/v1alpha1"
+	"github.com/domenicbove/angi/api/v1alpha1"
+	"github.com/domenicbove/angi/internal/podinfo"
+	"github.com/domenicbove/angi/internal/redis"
 )
 
 var _ = Describe("MyAppResource controller", func() {
@@ -31,26 +35,26 @@ var _ = Describe("MyAppResource controller", func() {
 
 		// cleanup myappresource
 		Eventually(func() error {
-			myApp := &myv1alpha1.MyAppResource{}
+			myApp := &v1alpha1.MyAppResource{}
 			k8sClient.Get(context.Background(), lookupKey, myApp)
 			return k8sClient.Delete(context.Background(), myApp)
 		}, timeout, interval).Should(Succeed())
 
 		Eventually(func() error {
-			myApp := &myv1alpha1.MyAppResource{}
+			myApp := &v1alpha1.MyAppResource{}
 			return k8sClient.Get(context.Background(), lookupKey, myApp)
 		}, timeout, interval).ShouldNot(Succeed())
 
 		// cleanup podinfo deployment
 		Eventually(func() error {
-			myApp := &appsv1.Deployment{}
-			k8sClient.Get(context.Background(), lookupKey, myApp)
-			return k8sClient.Delete(context.Background(), myApp)
+			podInfo := &appsv1.Deployment{}
+			k8sClient.Get(context.Background(), lookupKey, podInfo)
+			return k8sClient.Delete(context.Background(), podInfo)
 		}, timeout, interval).Should(Succeed())
 
 		Eventually(func() error {
-			myApp := &appsv1.Deployment{}
-			return k8sClient.Get(context.Background(), lookupKey, myApp)
+			podInfo := &appsv1.Deployment{}
+			return k8sClient.Get(context.Background(), lookupKey, podInfo)
 		}, timeout, interval).ShouldNot(Succeed())
 	})
 
@@ -61,7 +65,7 @@ var _ = Describe("MyAppResource controller", func() {
 
 			replicas := int32(2)
 
-			myAppResource := &myv1alpha1.MyAppResource{
+			myAppResource := &v1alpha1.MyAppResource{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "my.api.group/v1alpha1",
 					Kind:       "MyAppResource",
@@ -70,13 +74,13 @@ var _ = Describe("MyAppResource controller", func() {
 					Name:      MyAppResourceName,
 					Namespace: MyAppResourceNamespace,
 				},
-				Spec: myv1alpha1.MyAppResourceSpec{
+				Spec: v1alpha1.MyAppResourceSpec{
 					ReplicaCount: &replicas,
-					Image: myv1alpha1.Image{
+					Image: v1alpha1.Image{
 						Repository: "ghcr.io/stefanprodan/podinfo",
 						Tag:        "latest",
 					},
-					UI: myv1alpha1.UI{
+					UI: v1alpha1.UI{
 						Color:   "#34577c",
 						Message: "some message",
 					},
@@ -86,7 +90,7 @@ var _ = Describe("MyAppResource controller", func() {
 			Expect(k8sClient.Create(ctx, myAppResource)).Should(Succeed())
 
 			lookupKey := types.NamespacedName{Name: MyAppResourceName, Namespace: MyAppResourceNamespace}
-			createdMyAppResource := &myv1alpha1.MyAppResource{}
+			createdMyAppResource := &v1alpha1.MyAppResource{}
 
 			Eventually(func() bool {
 				err := k8sClient.Get(ctx, lookupKey, createdMyAppResource)
@@ -122,9 +126,9 @@ var _ = Describe("MyAppResource controller", func() {
 			Expect(*podInfoDeployment.Spec.Replicas).Should(Equal(int32(2)))
 			Expect(podInfoDeployment.Spec.Template.Spec.Containers[0].Name).Should(Equal("podinfo"))
 			Expect(podInfoDeployment.Spec.Template.Spec.Containers[0].Env).Should(ContainElement(
-				corev1.EnvVar{Name: "PODINFO_UI_COLOR", Value: "#34577c"}))
+				corev1.EnvVar{Name: podinfo.UIColorEnvVar, Value: "#34577c"}))
 			Expect(podInfoDeployment.Spec.Template.Spec.Containers[0].Env).Should(ContainElement(
-				corev1.EnvVar{Name: "PODINFO_UI_MESSAGE", Value: "some message"}))
+				corev1.EnvVar{Name: podinfo.UIMessageEnvVar, Value: "some message"}))
 			Expect(podInfoDeployment.Status.ReadyReplicas).Should(Equal(int32(0)))
 
 			By("By updating the podInfo deployment status")
@@ -148,7 +152,7 @@ var _ = Describe("MyAppResource controller", func() {
 			By("By creating a new MyAppResourceName without optional fields")
 			ctx := context.Background()
 
-			myAppResource := &myv1alpha1.MyAppResource{
+			myAppResource := &v1alpha1.MyAppResource{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "my.api.group/v1alpha1",
 					Kind:       "MyAppResource",
@@ -157,8 +161,8 @@ var _ = Describe("MyAppResource controller", func() {
 					Name:      MyAppResourceName,
 					Namespace: MyAppResourceNamespace,
 				},
-				Spec: myv1alpha1.MyAppResourceSpec{
-					UI: myv1alpha1.UI{
+				Spec: v1alpha1.MyAppResourceSpec{
+					UI: v1alpha1.UI{
 						Color:   "#34577c",
 						Message: "some message",
 					},
@@ -169,7 +173,7 @@ var _ = Describe("MyAppResource controller", func() {
 
 			By("By checking the created MyAppResource fields")
 			lookupKey := types.NamespacedName{Name: MyAppResourceName, Namespace: MyAppResourceNamespace}
-			createdMyAppResource := &myv1alpha1.MyAppResource{}
+			createdMyAppResource := &v1alpha1.MyAppResource{}
 
 			Eventually(func() bool {
 				err := k8sClient.Get(ctx, lookupKey, createdMyAppResource)
@@ -198,9 +202,88 @@ var _ = Describe("MyAppResource controller", func() {
 			Expect(*podInfoDeployment.Spec.Replicas).Should(Equal(int32(1)))
 			Expect(podInfoDeployment.Spec.Template.Spec.Containers[0].Name).Should(Equal("podinfo"))
 			Expect(podInfoDeployment.Spec.Template.Spec.Containers[0].Env).Should(ContainElement(
-				corev1.EnvVar{Name: "PODINFO_UI_COLOR", Value: "#34577c"}))
+				corev1.EnvVar{Name: podinfo.UIColorEnvVar, Value: "#34577c"}))
 			Expect(podInfoDeployment.Spec.Template.Spec.Containers[0].Env).Should(ContainElement(
-				corev1.EnvVar{Name: "PODINFO_UI_MESSAGE", Value: "some message"}))
+				corev1.EnvVar{Name: podinfo.UIMessageEnvVar, Value: "some message"}))
 		})
 	})
+
+	It("Should create MyAppResourceName with Redis", func() {
+		By("By creating a new MyAppResourceName with Redis Enabled")
+		ctx := context.Background()
+
+		myAppResource := &v1alpha1.MyAppResource{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "my.api.group/v1alpha1",
+				Kind:       "MyAppResource",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      MyAppResourceName,
+				Namespace: MyAppResourceNamespace,
+			},
+			Spec: v1alpha1.MyAppResourceSpec{
+				UI: v1alpha1.UI{
+					Color:   "#34577c",
+					Message: "some message",
+				},
+				Redis: &v1alpha1.Redis{
+					Enabled: true,
+				},
+			},
+		}
+
+		Expect(k8sClient.Create(ctx, myAppResource)).Should(Succeed())
+
+		By("By checking the created MyAppResource fields")
+		lookupKey := types.NamespacedName{Name: MyAppResourceName, Namespace: MyAppResourceNamespace}
+		createdMyAppResource := &v1alpha1.MyAppResource{}
+
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, lookupKey, createdMyAppResource)
+			if err != nil {
+				return false
+			}
+			return true
+		}, timeout, interval).Should(BeTrue())
+
+		Expect(createdMyAppResource.Spec.UI.Color).Should(Equal("#34577c"))
+		Expect(*createdMyAppResource.Spec.ReplicaCount).Should(Equal(int32(1)))
+		Expect(createdMyAppResource.Spec.Image.Repository).Should(Equal("ghcr.io/stefanprodan/podinfo"))
+		Expect(createdMyAppResource.Spec.Image.Tag).Should(Equal("latest"))
+
+		redisName := fmt.Sprintf("%s-redis", MyAppResourceName)
+		redisLookupKey := types.NamespacedName{Name: redisName, Namespace: MyAppResourceNamespace}
+
+		By("By checking the redis deployment fields")
+		redisDeployment := &appsv1.Deployment{}
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, redisLookupKey, redisDeployment)
+			if err != nil {
+				return false
+			}
+			return true
+		}, timeout, interval).Should(BeTrue())
+
+		// validate its fields!
+		Expect(redisDeployment.Name).Should(Equal(redisName))
+		Expect(redisDeployment.Spec.Template.Spec.Containers[0].Ports[0].ContainerPort).Should(Equal(int32(redis.RedisPort)))
+
+		By("By checking the redis service fields")
+		redisService := &corev1.Service{}
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, redisLookupKey, redisService)
+			if err != nil {
+				return false
+			}
+			return true
+		}, timeout, interval).Should(BeTrue())
+
+		// validate its fields!
+		Expect(redisService.Name).Should(Equal(redisName))
+		Expect(redisService.Spec.Ports[0].Name).Should(Equal("redis"))
+		Expect(redisService.Spec.Ports[0].Port).Should(Equal(int32(redis.RedisPort)))
+		Expect(redisService.Spec.Ports[0].TargetPort).Should(Equal(intstr.IntOrString{IntVal: redis.RedisPort}))
+		Expect(redisService.Spec.Selector).Should(Equal(map[string]string{"app": redisName}))
+	})
+
 })
